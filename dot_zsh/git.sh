bb580bc2c3dea189e5c88c24370a102cf8fce2c3
Denis Fetinin <denka0087@gmail.com>
2024-11-09 20:52:53 +0300

    gitconfig

--- dot_zsh/git.sh
+++ dot_zsh/git.sh
@@ -1,6 +1,22 @@
+# Check if main exists and use instead of master
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return 0
+    fi
+  done
+
+  # If no main branch was found, fall back to master but return error
+  echo master
+  return 1
+}
+
 alias gcam='git commit -a -m'
 alias gst='git status'
-alias gcan='git commit --ammend -no-edit'
+alias gcan='git commit --amend --no-edit'
 alias gp='git push'
 alias gpfl='git push --force-with-lease'
 alias ga='git add .'
